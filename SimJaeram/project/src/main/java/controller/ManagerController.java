8f048e3544375675de0fcc917098657c90566597
jectgenius <jkl9087@naver.com>
2024-04-29 23:26:56 +0900

    Feat: add logIn() 메소드 #9

--- SimJaeram/project/src/main/java/controller/ManagerController.java
+++ SimJaeram/project/src/main/java/controller/ManagerController.java
@@ -4,5 +4,9 @@ import repository.ManagerRepository;
 
 public class ManagerController {
     private ManagerRepository managerRepository = new ManagerRepository();
-    
+
+    public void logIn(String email, String password) {
+        managerRepository.logIn(email, password);
+    }
+
 }
